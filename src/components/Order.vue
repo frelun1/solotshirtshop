9d75d45a956ceb4b376e5fa81697686e719c8b04
Benny <benny_fow@hotmail.com>
2019-10-03 14:18:04 +0200

    första sida pill

--- src/components/Order.vue
+++ src/components/Order.vue
@@ -1,3 +1,11 @@
+<template>
+  <div class="orderComponent">
+    <h1>test</h1>
+  </div>
+</template>
+
+
+
 <script>
 export default {
   data() {
@@ -19,3 +27,21 @@ export default {
   }
 };
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
