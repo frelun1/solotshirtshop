2b470599dec4fc4b6a295cf7e629ae64942b2951
Benny <benny_fow@hotmail.com>
2019-10-03 13:41:36 +0200

    första sida start

---
+++ src/views/Order.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="order">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Order from '@/components/Order.vue'
+
+export default {
+  name: 'order',
+  components: {
+    Order
+  }
+}
+</script>
