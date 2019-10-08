28ebd472c18a9f6c88639abc0244503568077281
MaxEri1 <maxxen.me@gmail.com>
2019-10-08 19:07:48 +0200

    lagt till textconfig test samt komponent

---
+++ tests/unit/textconfig.spec.js
@@ -0,0 +1,40 @@
+import { mount } from '@vue/test-utils'
+import TextConfig from '@/components/TextConfig.vue'
+
+
+
+
+// Det ska gå att välja förvald text eller egen text på T-shirten, samt ändra storlek på texten.
+
+
+let expectedData={ TPreText: 'Text', TTextSize: {Width: 'Width', Heigth: 'Height'}, TTextColor: 'Color', TTextStyle: 'Style', TTextFont: 'Font', TMyText: 'MyText'}
+
+describe('Tshirt Text', () =>{
+    test('Mount unit', () => {
+        const wrapper = mount(TextConfig)
+        expect(wrapper.isVueInstance()).toBeTruthy()
+      })
+      test('Testing text configs', () => {
+        const wrapper = mount(TextConfig)
+        wrapper.vm.setTPreText(expectedData.TPreText)
+        expect(wrapper.vm.textConfig.TPreText).toEqual(expectedData.TPreText)
+
+        wrapper.vm.setTMyText(expectedData.TMyText)
+        expect(wrapper.vm.textConfig.TMyText).toEqual(expectedData.TMyText)
+
+        wrapper.vm.setTTextSize(expectedData.TTextSize)
+        expect(wrapper.vm.textConfig.TTextSize).toEqual(expectedData.TTextSize)
+
+        wrapper.vm.setTTextColor(expectedData.TTextColor)
+        expect(wrapper.vm.textConfig.TTextColor).toEqual(expectedData.TTextColor)
+        
+        wrapper.vm.setTTextStyle(expectedData.TTextStyle)
+        expect(wrapper.vm.textConfig.TTextStyle).toEqual(expectedData.TTextStyle)
+
+        wrapper.vm.setTTextFont(expectedData.TTextFont)
+        expect(wrapper.vm.textConfig.TTextFont).toEqual(expectedData.TTextFont)
+
+        wrapper.vm.setTMyText(expectedData.TMyText)
+        expect(wrapper.vm.textConfig.TMyText).toEqual(expectedData.TMyText)
+        })
+})
